666e775335b3a49f692e4c60f9280662df97906b
SóstenesRibeiro <sostenes.ribeiro@invillia.com>
2021-11-09 12:12:34 -0300

    Contemplação do projeto Fórum Alura

---
+++ forum/src/main/java/br/com/alura/forum/config/security/AutenticacaoViaTokenFilter.java
@@ -0,0 +1,51 @@
+package br.com.alura.forum.config.security;
+
+import br.com.alura.forum.modelo.Usuario;
+import br.com.alura.forum.repository.UsuarioRepository;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AutenticacaoViaTokenFilter extends OncePerRequestFilter {
+
+    private TokensService tokensService;
+    private UsuarioRepository repository;
+
+    public AutenticacaoViaTokenFilter(TokensService tokensService, UsuarioRepository repository) {
+        this.tokensService = tokensService;
+        this.repository = repository;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String token = recuperarToken(request);
+        boolean valido = tokensService.isTokenValido(token);
+        if (valido) {
+            autenticarCliente(token);
+        }
+        filterChain.doFilter(request, response);
+
+    }
+
+    private void autenticarCliente(String token) {
+        Long idUsuario = tokensService.getIdUsuario(token);
+        Usuario usuario = repository.findById(idUsuario).get();
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        String token = request.getHeader("Autorization");
+        if (token == null || token.isEmpty() || !token.startsWith("Bearer ")) {
+            return null;
+        }
+        return token.substring(7, token.length());
+    }
+}
